d89b6c746fca5360e4ce54a4e3cf58e2bbb0fe23
Archit Sharma <archit.sharma@cern.ch>
2013-07-31 22:25:34 +0200

    Changing output format and language

--- result_gen.py
+++ result_gen.py
@@ -1,7 +1,9 @@
 from invenio.invenio_connector import *
 from invenio.search_engine import  perform_request_search
 
-cds = InvenioConnector("http://cds.cern.ch")
+cds = InvenioConnector("http://cds.cern.ch/")
+#cds = InvenioConnector("http://pcuds54.cern.ch/")
+
 kwarg = raw_input("enter a search query: ")
 size = int(raw_input('Specify no. of records: '))
 
@@ -13,13 +15,14 @@ rec_id_arr = perform_request_search(p=kwarg,c=[""])
 
 '''To search inside CDS'''
 results = cds.search(p=kwarg,rg=size,ln='en')
+count=len(results)
 
 '''write results into a file'''
 file = open("10_def_op","w")
 
 file.write("Record ID's found for this search, in local instance on pcuds54 = "+str(rec_id_arr)+"\n\n")
 file.write("-----------------------------------------------------------------------------------------\n\n")
-file.write("First 10 results from CDS, for search string: '"+kwarg+"'\n")
+file.write("Showing "+str(count)+" result(s) found in CDS, for search string: '"+kwarg+"'\n")
 
 for i in xrange(len(results)):
     file.write("\n\n\t\t================================================================================\n\t\t\t\t\t\t-------- "+str(i+1)+" --------\n\t\t================================================================================\n")
