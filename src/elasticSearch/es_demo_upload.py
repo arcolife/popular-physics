a0273a0334065bfbf08e574fd1389311c7e84726
arcolife <arcshams@gmail.com>
2013-08-04 06:24:16 +0200

    fixed es_mapped.py

--- src/elasticSearch/es_demo_upload.py
+++ src/elasticSearch/es_demo_upload.py
@@ -1,18 +1,18 @@
 import json
 import pexpect
 
-file = open('../../../records.json','r')
+file = open('../../data/esReady_no_abstr.json','r')
 
 #demo =  '{ "authors": "Brady, M.Michael" , "free_keywords": [], "co-authors": ["Dedrick, Kent G."], "creation_date": "1962-09", "citations": 0, "references": 0, "papers": 1}'
 
 #print '\nDATA \n', data[data.keys()[0]]
 c=1
 for i in file:
-    query = "curl -XPUT 'http://localhost:9200/record/inspire/"+str(c)+"' -d '"+str(i.split("\n")[0])+"'"
-#    print "\nQUERY: ",query
+    query = "curl -XPUT 'http://localhost:9200/record/inspire/"+str(c)+"' -d '"+i.split("\n")[0]+"'"
+    print "\nQUERY: ",query
     result = pexpect.run(query)
-    print "\n",c," >> RESPONSE: ",result," <<"
-#    print c,">"
+    print "\nRESPONSE: \n",result,"\n"
+    print "\n===============================\n"
     c+=1
 
 #print curl_query
